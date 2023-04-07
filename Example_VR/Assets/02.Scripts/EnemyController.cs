2f2a6e984a8eb2f0a3333b151c412d37b98d5360
spirit790 <rkdqhdrnt@naver.com>
2023-04-07 15:48:36 +0900

    1차
    업

--- Example_VR/Assets/02.Scripts/EnemyController.cs
+++ Example_VR/Assets/02.Scripts/EnemyController.cs
@@ -9,10 +9,10 @@ public class EnemyController : MonoBehaviour
     {
         NONE=-1,
         IDEL=0,
-        WALK,
+        MOVE,
         ATTACK,
         DAMAGE,
-        DIE,
+        DEAD,
     }
     [Header("에너미상태")]
     public ENEMYSTATE enemyState;
@@ -25,40 +25,121 @@ public class EnemyController : MonoBehaviour
     public GameObject hitEffect;
     public GameObject item;
     [Header("Enemy")]
-    public float speed = 2f;
+    public float zombieSpeed = 2f;
     public int eHp = 5;
+    public float attackRange = 1.5f;
+    public float stateTime;
+    public float idleStateTime = 2f;
+    public float attackStateTime =1f;
+    public float damageStateTime = 1.5f;
+
+    [Header("LockOn")]
+    public bool isLockedOn = false;
+    public float lockTime;
+    public float lockCoolTime = 1f;
+
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         enemyState = ENEMYSTATE.IDEL;
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(enemyState !=ENEMYSTATE.DIE)
+        if(enemyState !=ENEMYSTATE.DEAD)
         switch (enemyState)
         {
             case ENEMYSTATE.NONE:
+                    GetComponent<BoxCollider>().enabled = false;
                 break;
             case ENEMYSTATE.IDEL:
+                    agent.speed = 0;
+                    anim.SetInteger("ENEMYSTATE", (int)enemyState);
+                    stateTime += Time.deltaTime;
+                    if(stateTime> idleStateTime)
+                    {
+                        stateTime = 0;
+                        enemyState = ENEMYSTATE.MOVE;
+                    }
+                    
                 break;
-            case ENEMYSTATE.WALK:
+            case ENEMYSTATE.MOVE:
+                    anim.SetInteger("ENEMYSTATE", (int)enemyState);
+                    agent.SetDestination(target.position);
+                    agent.speed = zombieSpeed;
+                    float dist = Vector3.Distance(target.position, transform.position);
+                    if(dist < attackRange)
+                    {
+                        enemyState = ENEMYSTATE.ATTACK;
+                    }
+                    else
+                    {
+                        agent.speed = zombieSpeed;
+                    }
                 break;
             case ENEMYSTATE.ATTACK:
+                    anim.SetInteger("ENEMYSTATE", (int)enemyState);
+                    agent.speed = 0;
+                    stateTime += Time.deltaTime;
+                    if(stateTime>attackStateTime)
+                    {
+                        stateTime = 0;
+                        Debug.Log("공격");
+                    }
                 break;
             case ENEMYSTATE.DAMAGE:
+                    anim.SetInteger("ENEMYSTATE", (int)enemyState);
+                    stateTime += Time.deltaTime;
+                    if(stateTime > damageStateTime)
+                    {
+                        stateTime = 0;
+                        enemyState = ENEMYSTATE.MOVE;
+                    }
                 break;
-            case ENEMYSTATE.DIE:
-                break;
+            case ENEMYSTATE.DEAD:
+                    Debug.Log("죽어");
+                    anim.SetTrigger("DEAD");
+                    enemyState = ENEMYSTATE.NONE;
+                    Destroy(gameObject, 2f);
+
+                    break;
             default:
                 break;
         }
-    }
-    public void MoveChange()
-    {
 
+        if(isLockedOn&&enemyState !=ENEMYSTATE.DEAD)
+        {
+            lockTime += Time.deltaTime;
+            if (lockTime > lockCoolTime)
+            {
+                lockTime = 0;
+                Instantiate(hitEffect, transform.position, transform.rotation);
+                enemyState = ENEMYSTATE.DAMAGE;
+                DamageByPlayer();
+            }
+        }
+    }
+    
+    public void DamageByPlayer()
+    {
+        --eHp;
+        if(eHp<=0)
+        {
+            enemyState = ENEMYSTATE.DEAD;
+        }
+    }
+
+    public void AimEnter()
+    {
+        isLockedOn = true;
+    }
+    public void AimExit()
+    {
+        isLockedOn = false;
+        lockTime = 0;
     }
 }
---
+++ Example_VR/Assets/02.Scripts/EnemyMaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMaker : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float curTime;
+    public float coolTime;
+
+    public int maxEnemy = 10;
+
+    public List<Vector3> spwanPoint;
+
+    public List<GameObject> enemyPool;
+
+
+    public void Start()
+    {
+        enemyPool = new List<GameObject>();
+        for (int i = 0; i < maxEnemy; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefab);
+            enemy.SetActive(false);
+            enemyPool.Add(enemy);
+        }
+    }
+    public void Update()
+    {
+        curTime += Time.deltaTime;
+
+    }
+    
+
+
+}
